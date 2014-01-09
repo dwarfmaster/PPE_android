7f3968a50ff3657a55a6bc4af03cedfdaa406513
lucas8 <luc.linux@mailoo.org>
2014-01-09 08:38:26 +0100

    Initial commit.

---
+++ src/ppe/abitbol/main/PPE.java
@@ -0,0 +1,15 @@
+package ppe.abitbol.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PPE extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
